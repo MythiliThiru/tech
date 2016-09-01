af9976a2ccd26de6cebc4af11463c77d11ec7d1e
MythiliThiru <thiru.mythili@gmail.com>
2016-09-01 18:16:55 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class num
+{
+public static void main(String a[])
+{
+int n=7;
+if(n>0)
+{
+System.out.println("positive");
+}
+else if(n<0)
+{
+system.out.println("negative");
+}
+else
+{
+System.out.println("zero");
+}
+}
+}
