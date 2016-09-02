fa0fb1a31b9361c480e75f7049f7635f48685f1f
MythiliThiru <thiru.mythili@gmail.com>
2016-09-02 11:26:45 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,16 @@
+import.java.io.*;
+void main()
+{
+public static void main()
+{
+char c='g';
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+System.out.println("vowels");
+}
+else
+{
+System.out.println("consonants");
+}
+}
+}
